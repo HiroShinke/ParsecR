02a8c003e03c0baad89a51079f083a61023fd222
hirofumi <hiro.shinke@gmail.com>
2017-01-27 22:37:48 +0900

    *** empty log message ***

---
+++ schemeenv.rb
@@ -0,0 +1,42 @@
+
+
+
+class Env
+
+  attr :parent
+  attr_accessor :dict
+  
+  def initialize(par = nil)
+    @parent = par
+    @dict = {}
+  end
+  
+  def get(k)
+    if dict.key?(k)
+      dict[k]
+    elsif parent
+      parent.get(k)
+    else
+      nil
+    end
+  end
+
+  def set(k,v)
+    if dict.key?(k)
+      dict[k] = v
+    elsif parent
+      parent.set(k,v)
+    else
+      raise "variable not defined!!"
+    end
+  end
+
+  def define(k,v)
+    if dict.key?(k)
+      raise "variable already defined!!"
+    else
+      dict[k] = v
+    end
+  end
+
+end
